b08583634fe2b72382cf31d8366752d8d9e7ddc8
chaewon5205 <parkjihae9262@gmail.com>
2026-07-02 17:23:56 +0900

    feat: 오늘 배운 것 최종 커밋

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //json->object
+
+let corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "bbs",
+});
+
+db.connect();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/write", (req, res) => {
+  console.log(req.body);
+  const { title, name, content } = req.body;
+
+  const sqlQuery = "insert into board (title,content,writer) values (?,?,?);";
+  db.query(sqlQuery, [title, content, name], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
